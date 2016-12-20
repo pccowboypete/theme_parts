69f3455b637941bbde2431a7184758dcbba5f979
Peter Nanayon <pccowboyphils@gmail.com>
2016-12-20 22:46:21 +0800

    Added files

---
+++ blank/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta name="robots" content="index, follow">
+	
+	<!-- Box reset stylesheet -->
+	<link rel="stylesheet" type="text/css" href="assets/border-box-reset.css">
+	
+	<!-- Bootstrap stylesheet -->
+	<link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.css">
+	
+	<!-- Default stylesheet -->
+	<link rel="stylesheet" type="text/css" href="style.css">
+    	
+    
+    <title>Test</title>
+</head>
+
+<body>
+	<!-- Header -->
+	<header>
+	</header>    
+	
+	<!-- Section -->
+	<section>
+		<button class="btn btn-lg btn-primary">Hello there</button>
+	</section>
+	
+	<!-- Footer -->
+    <footer class="text-center">
+
+    </footer>
+
+    
+    <!-- Inlcude jQuery and Bootstrap START -->
+	<script src="assets/jquery.js"></script>
+	<script src="assets/bootstrap/js/bootstrap.js"></script>
+	<!-- Inlcude jQuery and Bootstrap END -->
+
+	<!-- Use HTML5shiv.js and respond.js Fix if browser is IE9 or below -->
+		<!--[if lte IE 9]>
+			<script src="assets/html5shiv.js"></script>
+			<script src="assets/respond.js"></script>
+		<![endif]-->
+	<!-- END HTML5shiv.js and Respond.js -->
+    
+
+</body>
+
+</html>
+    
